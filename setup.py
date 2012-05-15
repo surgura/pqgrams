23b331806a5d938cea02f6ffdff9768432fb5301
Tim Henderson <tim.tadh@gmail.com>
2012-05-14 22:32:01 -0400

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='pygram',
+      version='git master',
+      description=(
+        'An efficient approximation for tree edit-distance.'
+      ),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/PyGram',
+      license='BSD',
+      packages=['pygram'],
+)
+
