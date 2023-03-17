8918709190d17ad910c4b01ca72cb3bdc67d08a3
Aart Stuurman <astuurman@hotmail.com>
2023-03-17 15:27:31 +0100

    set version to 0.0.0 to fix installation

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
 
-setup(name='pqgrams',
-      version='git master',
-      description=(
-        'An efficient approximation for tree edit-distance.'
-      ),
-      author='Tim Henderson',
-      author_email='tim.tadh@gmail.com',
-      url='https://www.github.com/timtadh/PyGram',
-      license='BSD',
-      packages=['pqgrams'],
+setup(
+    name="pqgrams",
+    version="v0.0.0",
+    description=("An efficient approximation for tree edit-distance."),
+    author="Tim Henderson",
+    author_email="tim.tadh@gmail.com",
+    url="https://www.github.com/timtadh/PyGram",
+    license="BSD",
+    packages=["pqgrams"],
 )
-
